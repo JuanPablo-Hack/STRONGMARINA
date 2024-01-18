e65d24ded13b231020c087ac4a2bfadbd8412991
Mona Lisa <sistemas@porter.com.mx>
2024-01-18 15:13:38 -0600

    test

--- contactform/php/sendEmail.php
+++ contactform/php/sendEmail.php
@@ -1,48 +1,181 @@
 ﻿<?php
+enviar_email($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message']);
+function enviar_email($nombre, $correo, $asunto, $mensaje)
+{
+  $destinatario = 'juanpablodejesus_figueroa@gmail.com';
+  $cuerpo = ' 
+  <!DOCTYPE html>
+  <html
+    lang="en"
+    xmlns="http://www.w3.org/1999/xhtml"
+    xmlns:o="urn:schemas-microsoft-com:office:office"
+  >
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="x-apple-disable-message-reformatting" />
+      <title></title>
+      <style>
+        table,
+        td,
+        div,
+        h1,
+        p {
+          font-family: Arial, sans-serif;
+        }
+        @media screen and (max-width: 530px) {
+          .unsub {
+            display: block;
+            padding: 8px;
+            margin-top: 14px;
+            border-radius: 6px;
+            background-color: #555555;
+            text-decoration: none !important;
+            font-weight: bold;
+          }
+          .col-lge {
+            max-width: 100% !important;
+          }
+        }
+        @media screen and (min-width: 531px) {
+          .col-sml {
+            max-width: 27% !important;
+          }
+          .col-lge {
+            max-width: 73% !important;
+          }
+        }
+      </style>
+    </head>
+    <body
+      style="
+        margin: 0;
+        padding: 0;
+        word-spacing: normal;
+        background-color: #ffffff;
+      "
+    >
+      <div
+        role="article"
+        aria-roledescription="email"
+        lang="en"
+        style="
+          text-size-adjust: 100%;
+          -webkit-text-size-adjust: 100%;
+          -ms-text-size-adjust: 100%;
+          background-color: #fff;
+        "
+      >
+        <table
+          role="presentation"
+          style="width: 100%; border: none; border-spacing: 0"
+        >
+          <tr>
+            <td align="center" style="padding: 0">
+              <table
+                role="presentation"
+                style="
+                  width: 94%;
+                  max-width: 600px;
+                  border: none;
+                  border-spacing: 0;
+                  text-align: left;
+                  font-family: Arial, sans-serif;
+                  font-size: 16px;
+                  line-height: 22px;
+                  color: #363636;
+                "
+              >
+                <tr>
+                  <td
+                    style="
+                      padding: 40px 30px 30px 30px;
+                      text-align: center;
+                      font-size: 24px;
+                      font-weight: bold;
+                    "
+                  >
+                    <a
+                      href="https://gruposoca.com.mx/"
+                      style="text-decoration: none"
+                      ><img
+                        src="http://twologistic.com/images/twologistic.png"
+                        width="165"
+                        alt="Logo"
+                        style="
+                          width: 165px;
+                          max-width: 80%;
+                          height: auto;
+                          border: none;
+                          text-decoration: none;
+                          color: #ffffff;
+                        "
+                    /></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="padding: 30px; background-color: #ffffff">
+                    <h1
+                      style="
+                        margin-top: 0;
+                        margin-bottom: 16px;
+                        font-size: 26px;
+                        line-height: 32px;
+                        font-weight: bold;
+                        letter-spacing: -0.02em;
+                      "
+                    >
+                      Te ha llegado un mensaje desde tu sitio web!
+                    </h1>
+                    <p style="margin: 0">
+                      Por medio del presente correo le decimos que la persona ' . $nombre . ' requiere información acerca de alguna cotización, el cual tiene un corre que es el siguiente
+                      <a
+                        href="#"
+                        style="color: #ff2b06; text-decoration: underline"
+                        >' . $correo . '</a
+                      >, por lo que su mensaje enviado y su pregunta es: <br>
+                      ' . $mensaje . '
+                    </p>
+                    <br>
 
-// Replace this with your own email address
-$to = 'juanpablodejesusfigueroa@gmail.com';
 
-function url(){
-  return sprintf(
-    "%s://%s",
-    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-    $_SERVER['SERVER_NAME']
-  );
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td
+                    style="
+                      padding: 30px;
+                      text-align: center;
+                      font-size: 12px;
+                      background-color: #2B3F54;
+                      color: #fff;
+                    "
+                  >
+                    <p style="margin: 0; font-size: 14px; line-height: 20px">
+                      &reg; Twologistic, todos los derechos reservados 2022<br /><a
+                        class="unsub"
+                        href="https://twologistic.com/"
+                        style="color: #fff; text-decoration: underline"
+                        >www.twologistic.com</a
+                      >
+                    </p>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </body>
+  </html>
+  ';
+  //para el envío en formato HTML 
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+  //dirección del remitente 
+  $headers .= "From: Sitio web <sistemas@twologistic.com>\r\n";
+  mail($destinatario, $asunto, $cuerpo, $headers);
+  header("Location: ../contact.html");
 }
-
-if($_POST) {
-
-   $name = trim(stripslashes($_POST['name']));
-   $email = trim(stripslashes($_POST['email']));
-   $subject = trim(stripslashes($_POST['subject']));
-   $contact_message = trim(stripslashes($_POST['message']));
-
-   
-	if ($subject == '') { $subject = "Contact Form Submission"; }
-
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	 $message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= nl2br($contact_message);
-   $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
-
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
-
-   // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-   ini_set("sendmail_from", $to); // for windows server
-   $mail = mail($to, $subject, $message, $headers);
-
-	if ($mail) { echo "OK"; }
-   else { echo "Something went wrong. Please try again."; }
-
-}
-
-?>
